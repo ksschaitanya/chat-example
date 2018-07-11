cfc1fde3303262399a07116ef780475d806805ce
Kocherlakota <srkocherlakota@deloitte.com>
2018-07-11 15:29:36 -0400

    new file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
   //res.sendFile(__dirname + '/index.html');
   //res.sendFile(__dirname + '/indexNewFeatures.html');
   //res.sendFile(__dirname + '/btindexnew.html');
-  res.sendFile(__dirname + '/btindexnewV2.html');
+  res.sendFile(__dirname + '/btindexnewV3.html');
 });
 
 io.on('connection', function(socket){
