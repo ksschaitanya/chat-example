0b993b7c3fa374031b29916f1a119e1ecc2ca7e7
Kocherlakota <srkocherlakota@deloitte.com>
2018-08-08 16:46:25 -0400

    JWT

--- index.js
+++ index.js
@@ -13,6 +13,8 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var bodyParser= require('body-parser');
+var jwt=require('jsonwebtoken');
 
 var crypto = require('crypto'),
 algorithm = 'aes-256-ctr',
@@ -86,3 +88,90 @@ function decrypt(text){
   dec += decipher.final('utf8');
   return dec;
 }
+
+var users=[
+  {
+    name:"xxxx",
+    password:"xxxx"
+  },
+  {
+    name:"yyyy",
+    password:"yyyy"
+  }
+  ];
+  app.use( bodyParser.json() );
+  app.use(bodyParser.urlencoded({
+      extended: true
+  }));
+  
+  app.use(express.static('./'));
+  
+  app.get('/', (req,res)=>{
+      res.sendFile('index.html');
+  });
+  
+  app.post('/login',(req,res)=>{
+      var message;
+      for(var user of users){
+        if(user.name!=req.body.name){
+            message="Wrong Name";
+        }else{
+            if(user.password!=req.body.password){
+                message="Wrong Password";
+                break;
+            }
+            else{
+                var token=jwt.sign(user,"samplesecret");
+                console.log(token);
+                message="Login Successful";
+                break;
+            }
+        }
+      }
+      if(token){
+          res.status(200).json({
+              message,
+              token
+          });
+      }
+      else{
+          res.status(403).json({
+              message
+          });
+      }
+  });
+  
+  app.use((req, res, next)=>{
+          // check header or url parameters or post parameters for token
+          console.log(req.body);
+          var token = req.body.token || req.query.token || req.headers['x-access-token'];
+          if(token){
+            console.log("token");
+            jwt.verify(token,"samplesecret",(err,decod)=>{
+              if(err){
+                res.status(403).json({
+                  message:"Wrong Token"
+                });
+              }
+              else{
+                console.log("success");
+                req.decoded=decod;
+                next();
+              }
+            });
+          }
+          else{
+            res.status(403).json({
+              message:"No Token"
+            });
+          }
+  });
+  
+  app.post('/getusers',(req,res)=>{
+      var user_list=[];
+      console.log("here");
+      users.forEach((user)=>{
+          user_list.push({"name":user.name});
+      })
+      res.send(JSON.stringify({users:user_list}));
+  });
